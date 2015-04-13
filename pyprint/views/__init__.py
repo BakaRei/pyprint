4a91b32dbc24e5d57567eb4e93f7d28744df36d9
BakaRei <8akarei@gmail.com>
2015-04-13 16:33:46 +0800

    hook test orz

--- pyprint/views/__init__.py
+++ pyprint/views/__init__.py
@@ -28,7 +28,7 @@ handlers = [
     (r'/kamisama/posts/add', AddPostHandler),
     (r'/kamisama/links', AddLinkHandler),
 
-    #webhook.py
+    #webhook.py hook test orz
     (r'/hook', WebHookHandler),
 
     # others.py
